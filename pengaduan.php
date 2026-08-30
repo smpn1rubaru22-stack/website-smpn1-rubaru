9e5be53daf9d1f51322a19388c979022e7f5448e
Yuliana Wardani <smpn1.rubaru21@gmail.com>
2026-08-30 17:38:01 +0700

    Tambahkan favicon logo sekolah

--- pengaduan.php
+++ pengaduan.php
@@ -21,7 +21,7 @@
         rel="stylesheet">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="assets/css/style.css?v=4">
+    <link rel="stylesheet" href="assets/css/style.css?v=5">
 
 </head>
 <body>
