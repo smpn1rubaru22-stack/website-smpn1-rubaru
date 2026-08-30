42b0906ee83caebe8a724ae77bb1c99eb0f90714
Yuliana Wardani <smpn1.rubaru21@gmail.com>
2026-08-30 14:19:57 +0700

    Perbaiki posisi header berita

--- berita.php
+++ berita.php
@@ -51,7 +51,7 @@ LIMIT $mulai,$batas");
 
 <body>
 <?php include "partial/navbar.php"; ?>
-<main style="padding-top: 50px 0 45px;">
+<main style="padding-top: 45px;">
 <section class="header-berita">
 
     <div class="container">
